6790782b0d183ea30e9aab52a24133a977407bc3
gengzhaoji <1317993594@qq.com>
2020-11-11 10:24:24 +0800

    Signed-off-by: gengzhaoji <1317993594@qq.com>端口改为5000

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
   devServer: {
     open: process.platform === 'darwin',
     host: '0.0.0.0',
-    port: 9001,
+    port: 5000,
     https: false,
     hotOnly: false,
     // proxy: {// 设置代理
